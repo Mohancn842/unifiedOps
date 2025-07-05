08bfbf34f8ac87ed7283ef06b235c8cd3c61e5a5
karthiksr-7 <karthiksrao777@gmail.com>
2025-07-05 12:36:49 +0530

    Update managerApi.js

--- frontend/src/services/managerApi.js
+++ frontend/src/services/managerApi.js
@@ -3,7 +3,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+baseURL: 'https://unifiedops-backend.onrender.com/api',
+
 });
 
 API.interceptors.request.use((config) => {
